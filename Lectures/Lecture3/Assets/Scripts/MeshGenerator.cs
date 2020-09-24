44ea6a75caedc2e7a84953c5a7fa21d0f019a435
Konstantin Lyubort <lyubortk@gmail.com>
2020-09-24 13:25:25 +0300

    finish hw1

--- Lectures/Lecture3/Assets/Scripts/MeshGenerator.cs
+++ Lectures/Lecture3/Assets/Scripts/MeshGenerator.cs
@@ -1,10 +1,18 @@
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Mathematics;
 
 [RequireComponent(typeof(MeshFilter))]
 public class MeshGenerator : MonoBehaviour
 {
     public MetaBallField Field = new MetaBallField();
+
+    public Vector3 farCorner;
+    public float boundingCubeEdge;
+    public float smallCubeEdge;
+    public float normalApproximationDelta;
+
+    private float eps = 0.0001F;
     
     private MeshFilter _filter;
     private Mesh _mesh;
@@ -33,64 +41,69 @@ public class MeshGenerator : MonoBehaviour
     /// You can use it to animate something in runtime.
     /// </summary>
     private void Update()
-    {
-        List<Vector3> cubeVertices = new List<Vector3>
-        {
-            new Vector3(0, 0, 0), // 0
-            new Vector3(0, 1, 0), // 1
-            new Vector3(1, 1, 0), // 2
-            new Vector3(1, 0, 0), // 3
-            new Vector3(0, 0, 1), // 4
-            new Vector3(0, 1, 1), // 5
-            new Vector3(1, 1, 1), // 6
-            new Vector3(1, 0, 1), // 7
-        };
-
-        int[] sourceTriangles =
-        {
-            0, 1, 2, 2, 3, 0, // front
-            3, 2, 6, 6, 7, 3, // right
-            7, 6, 5, 5, 4, 7, // back
-            0, 4, 5, 5, 1, 0, // left
-            0, 3, 7, 7, 4, 0, // bottom
-            1, 5, 6, 6, 2, 1, // top
-        };
-
-        
+    {   
         vertices.Clear();
         indices.Clear();
         normals.Clear();
         
         Field.Update();
-        // ----------------------------------------------------------------
-        // Generate mesh here. Below is a sample code of a cube generation.
-        // ----------------------------------------------------------------
 
-        // What is going to happen if we don't split the vertices? Check it out by yourself by passing
-        // sourceVertices and sourceTriangles to the mesh.
-        for (int i = 0; i < sourceTriangles.Length; i++)
-        {
-            indices.Add(vertices.Count);
-            Vector3 vertexPos = cubeVertices[sourceTriangles[i]];
-            
-            //Uncomment for some animation:
-            //vertexPos += new Vector3
-            //(
-            //    Mathf.Sin(Time.time + vertexPos.z),
-            //    Mathf.Sin(Time.time + vertexPos.y),
-            //    Mathf.Sin(Time.time + vertexPos.x)
-            //);
-            
-            vertices.Add(vertexPos);
+        for (float dx = 0; dx + smallCubeEdge <= boundingCubeEdge + eps; dx += smallCubeEdge) {
+            for (float dy = 0; dy + smallCubeEdge <= boundingCubeEdge + eps; dy += smallCubeEdge) {
+                for (float dz = 0; dz + smallCubeEdge <= boundingCubeEdge + eps; dz += smallCubeEdge) {
+                    Vector3 smallCubeCorner = farCorner + new Vector3(dx, dy, dz);
+                    
+                    int caseIndex = 0;
+                    for (int vertexIndex = 0; vertexIndex < 8; vertexIndex++) {
+                        Vector3 vertexPosition = smallCubeCorner + MarchingCubes.Tables._cubeVertices[vertexIndex] * smallCubeEdge;
+                        float f = Field.F(vertexPosition);
+                        if (f > 0) {
+                            caseIndex |= 1 << vertexIndex;
+                        }
+                    }
+
+                    int numberOfTriangles = MarchingCubes.Tables.CaseToTrianglesCount[caseIndex];
+
+                    for (int triangleNum = 0; triangleNum < numberOfTriangles; triangleNum++) {
+                        int3 edges = MarchingCubes.Tables.CaseToVertices[caseIndex][triangleNum];
+                        processEdge(edges.x, smallCubeCorner);
+                        processEdge(edges.y, smallCubeCorner);
+                        processEdge(edges.z, smallCubeCorner);
+                    }
+
+                }
+            }
         }
 
         // Here unity automatically assumes that vertices are points and hence (x, y, z) will be represented as (x, y, z, 1) in homogenous coordinates
         _mesh.Clear();
         _mesh.SetVertices(vertices);
         _mesh.SetTriangles(indices, 0);
-        _mesh.RecalculateNormals(); // Use _mesh.SetNormals(normals) instead when you calculate them
+        _mesh.SetNormals(normals);
 
         // Upload mesh data to the GPU
         _mesh.UploadMeshData(false);
+    }
+
+    private void processEdge(int edgeNum, Vector3 smallCubeCorner) {
+        int[] edgeVertices = MarchingCubes.Tables._cubeEdges[edgeNum];
+        Vector3 point1 = smallCubeCorner + MarchingCubes.Tables._cubeVertices[edgeVertices[0]] * smallCubeEdge;
+        Vector3 point2 = smallCubeCorner + MarchingCubes.Tables._cubeVertices[edgeVertices[1]] * smallCubeEdge;
+        float f1 = Field.F(point1);
+        float f2 = Field.F(point2);
+        Vector3 trianglePoint = Vector3.Lerp(point1, point2, -f1 / (f2 - f1)); 
+
+        indices.Add(vertices.Count);
+        vertices.Add(trianglePoint);
+
+        Vector3 dx = new Vector3(normalApproximationDelta, 0, 0);
+        Vector3 dy = new Vector3(0, normalApproximationDelta, 0);
+        Vector3 dz = new Vector3(0, 0, normalApproximationDelta);
+
+        Vector3 normal = -1 * Vector3.Normalize(new float3(
+            Field.F(trianglePoint + dx) - Field.F(trianglePoint - dx),
+            Field.F(trianglePoint + dy) - Field.F(trianglePoint - dy),
+            Field.F(trianglePoint + dz) - Field.F(trianglePoint - dz)));
+        normals.Add(normal);
     }
 }
